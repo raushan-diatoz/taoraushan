3e1f82e57800c8d25b546e6a4e768fc0502352e3
Raushandiatoz <raushan.kumar@diatoz.com>
2019-12-30 13:34:46 +0530

    raushan its

---
+++ manifest.php
@@ -0,0 +1,59 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */
+
+/**
+ * Generated using taoDevTools 2.21.0
+ */
+return array(
+    'name' => 'taoraushan',
+    'label' => 'Fine',
+    'description' => 'nothing',
+    'license' => 'GPL-2.0',
+    'version' => '2.21.0',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'generis' => '>=6.7.0'
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoraushanManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoraushanManager', array('ext'=>'taoraushan')),
+    ),
+    'install' => array(
+    ),
+    'uninstall' => array(
+    ),
+    'routes' => array(
+        '/taoraushan' => 'oat\\taoraushan\\controller'
+    ),    
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+        
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'taoraushan/',
+        
+        #BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL.'taoraushan/views/'
+    ),
+    'extra' => array(
+        'structures' => dirname(__FILE__).DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'structures.xml',
+    )
+);
